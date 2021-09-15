0716fc23b2a8c51c7afedc6fd38df857b1780682
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-15 19:14:48 -0300

    add matched chars display

--- src/components/Word/index.js
+++ src/components/Word/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Word = ({ word, validKeys }) => {
+  if (!word) return null;
+
+  const matched = word.slice(0, validKeys.length);
+  const remainder = word.slice(validKeys.length);
+
   return (
     <>
-      <span className="matched"></span>
-      <span className="unmatched">{ word }</span>
+      <span className="matched">{ matched }</span>
+      <span className="unmatched">{ remainder }</span>
     </>
   );
 };
