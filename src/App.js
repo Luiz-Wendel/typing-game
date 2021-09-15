c0626eac0fade56a49fed93b54c7cd2a49327188
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-15 18:43:05 -0300

    add typed keys

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 
+const MAX_TYPED_KEYS = 30;
+
 const App = () => {
+  const [typedKeys, setTypedKeys] = React.useState([]);
+
+  const handleKeyDown = (event) => {
+    const { key } = event;
+
+    event.preventDefault();
+
+    setTypedKeys((previousTypesKeys) => [...previousTypesKeys, key].slice(-MAX_TYPED_KEYS));
+  };
+
   return (
-    <section className="container">
+    <section className="container" tabIndex="0" onKeyDown={ handleKeyDown }>
       <section className="validKeys">
         <span className="matched">Xa</span>
         <span className="unmatched">blau</span>
       </section>
-      <section className="typedKeys">arewgfaer</section>
+      <section className="typedKeys">
+        { typedKeys && typedKeys }
+      </section>
       <section className="completedWords">
         <ol>
           <li>Biscoito</li>
